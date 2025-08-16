b34451677c2b13f713b371dc6968a92b880308aa
Uzinex <uzdilovferuz@gmail.com>
2025-08-16 23:58:09 +0500

    Checkin one

--- core/settings.py
+++ core/settings.py
@@ -61,9 +61,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = []
